cfb434caea59eff5bb46c68039e33e3ba4c8d5c1
teruki <terubouzu0628@gmail.com>
2020-06-08 06:53:35 +0000

    add new file

---
+++ practice.php
@@ -0,0 +1,29 @@
+<?php
+echo 'hello php!';
+$a = 3 ;
+$b = 7 ;
+echo $a + $b ;
+$array_month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+echo $array_month[9];
+$hello = " Hello, ";
+$name = " Teruki, ";
+$world = " 's World";
+echo $hello .$name . $world ;
+$tech_boost = "tech" ;
+$tech_boost .= "boost" ;
+echo $tech_boost ;
+$new2018_calendar = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+echo $new2018_calendar[December];
