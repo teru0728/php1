97491477be593f98760e2e21c3a1088241135056
teruki <terubouzu0628@gmail.com>
2020-11-10 08:08:06 +0000

    add new file

---
+++ practice4.php
@@ -0,0 +1,29 @@
+<?php
+$name = "福田晃生" ;
+if ($name != "福田晃生") {
+    echo "福田晃生ではありません";
+} else  {
+    echo "私は福田晃生です";
+}
+$total = 0;
+for ($i = 0; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
+$fruits = array("apple", "cherry","pineapple","pear","peach");
+foreach($fruits as $fruits){
+    echo "要素は". $fruits;
+    echo "\n";
+}
+/* for文の始めの値を定義する */
+$start = 1;
+/* for文の終わりの値を定義する */
+$end = 100;
+
+for($i = $start; $i <= $end; $i++){
+
+  // 5で割り切れたら{}内を実行する
+  if($i % 5 == 0){
+    echo $i;
+  }
+}
